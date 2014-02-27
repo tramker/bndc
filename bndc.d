2067cce421ad8cbc19ac2e165c371159a022cef8
Martin Krejcirik <mk@krej.cz>
2014-02-27 02:12:11 +0100

    bndc: defaults

--- bndc.d
+++ bndc.d
@@ -1,6 +1,27 @@
 import std.stdio, std.file, std.functional;
 import vars, cmds, eparser, gen;
 
+void setDefaults()
+{
+	var["template_suffix"] = ".tpl";
+	var["template_dir"] = ".";
+	var["zone_suffix"] = ".db";
+	var["zone_dir"] = ".";
+	var["version_suffix"] = ".ver";
+	var["header"] = "header.tpl";
+	var["footer"] = "footer.tpl";
+	var["ttl"] = "1d";
+	var["refresh"] = "4h";
+	var["retry"] = "1h";
+	var["expire"] = "30d";
+	var["negttl"] = "15m";
+	var["nsname"] = "localhost.";
+	var["maintname"] = "root.localhost.";
+	var["cmd_reload"] = "rndc reload";
+	var["cmd_check"] = "/usr/sbin/named-checkzone !zone !file";
+}
+
+
 void main(string[] args)
 {
 	string filename;
@@ -8,6 +29,8 @@ void main(string[] args)
 		filename = args[1];
 	else
 		filename = "config.tpl";
+		
+	setDefaults();
 
 	parser = new EParser;
 	parser.onSet = &var.put;   /* run whe variable is set (!var=somthing) */
